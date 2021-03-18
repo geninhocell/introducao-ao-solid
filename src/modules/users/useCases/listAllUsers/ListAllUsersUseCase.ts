4b0af6a9ab4b185f7a525120d9da563ba4c41428
geninhocell <geninhocell@gmail.com>
2021-03-18 18:10:50 -0300

    completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin) {
+      throw new Error("Mensagem do erro");
+    }
+
+    if (!userAdmin.admin) {
+      throw new Error("Mensagem do erro");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
